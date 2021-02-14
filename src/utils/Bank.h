730580833ad9b0eb42bac0ce3669b6e83ebb39ec
gitpeterwind <peter.wind@met.no>
2021-02-14 10:35:20 +0100

    New Bank

---
+++ src/utils/Bank.h
@@ -0,0 +1,92 @@
+#pragma once
+
+#include "MRCPP/Parallel"
+
+#include "/home/peter/mrchem/src/mrchem.h"
+#include "/home/peter/mrchem/src/parallel.h"
+#include "/home/peter/mrchem/src/qmfunctions/qmfunction_fwd.h"
+
+namespace mrchem {
+
+using namespace mpi;
+
+struct deposit {
+    Orbital *orb;
+    double *data; // for pure data arrays
+    bool hasdata;
+    int datasize;
+    int id = -1; // to identify what is deposited
+    int source;  // mpi rank from the source of the data
+};
+
+struct queue_struct {
+    int id;
+    std::vector<int> clients;
+};
+    int const CLOSE_BANK = 1;
+    int const CLEAR_BANK = 2;
+    int const NEW_ACCOUNT = -1;
+    int const CLOSE_ACCOUNT = 0;
+    int const GET_ORBITAL = 3;
+    int const GET_ORBITAL_AND_WAIT = 4;
+    int const GET_ORBITAL_AND_DELETE = 5;
+    int const SAVE_ORBITAL = 6;
+    int const GET_FUNCTION = 7;
+    int const SAVE_FUNCTION = 8;
+    int const SET_DATASIZE = 9;
+    int const GET_DATA = 10;
+    int const SAVE_DATA = 11;
+    int const SAVE_NODEDATA = 12;
+    int const GET_NODEDATA = 13;
+    int const GET_NODEBLOCK = 14;
+    int const GET_ORBBLOCK = 15;
+    int const CLEAR_BLOCKS = 16;
+    int const GETMAXTOTDATA = 17;
+    int const GETTOTDATA = 18;
+
+class CentralBank {
+public:
+    CentralBank() = default;
+    ~CentralBank();
+    void open();
+    void close();
+    int openAccount(int iclient, MPI_Comm comm);
+    void closeAccount(int account_id) ;
+    long long totcurrentsize = 0ll; // number of kB used by all accounts
+private:
+    std::vector<int> accounts; // open bank accounts
+    std::map<int, std::vector<deposit>*> get_deposits; // gives deposits of an account
+    std::map<int, std::map<int, int>*> get_id2ix;
+    std::map<int, std::map<int, int>*> get_id2qu;
+    std::map<int, std::vector<queue_struct>*> get_queue; // gives deposits of an account
+    std::map<int,long long> currentsize; // total deposited data size (without containers)
+    long long maxsize = 0;     // max total deposited data size (without containers)
+    void clear_bank();
+    void clear_account(int account);
+    int get_maxtotalsize();
+    std::vector<int> get_totalsize();
+};
+
+class BankAccount {
+public:
+    BankAccount(int iclient = orb_rank, MPI_Comm comm = comm_orb);
+    ~BankAccount();
+    int account_id = -1;
+    int put_orb(int id, Orbital &orb);
+    int get_orb(int id, Orbital &orb, int wait = 0);
+    int get_orb_del(int id, Orbital &orb);
+    int put_func(int id, QMFunction &func);
+    int get_func(int id, QMFunction &func);
+    void set_datasize(int datasize, int iclient = orb_rank, MPI_Comm comm = comm_orb);
+    int put_data(int id, int size, double *data);
+    int get_data(int id, int size, double *data);
+    int put_nodedata(int id, int nodeid, int size, double *data);
+    int get_nodedata(int id, int nodeid, int size, double *data, std::vector<int> &idVec);
+    int get_nodeblock(int nodeid, double *data, std::vector<int> &idVec);
+    int get_orbblock(int orbid, double *&data, std::vector<int> &nodeidVec, int bankstart);
+    void clear_blockdata(int i = orb_rank, int nodeidmax = 0, MPI_Comm comm = comm_orb);
+};
+
+int const message_size = 5;
+
+} // namespace mrchem
